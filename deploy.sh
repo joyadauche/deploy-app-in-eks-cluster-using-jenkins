b2c58ab284f00412eca8b6298e2c12950c30526f
Joy Uchenna <joy.adaeze.uchenna@gmail.com>
2024-01-29 15:00:49 -0500

    Update filepath in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed -i "s#placeholder-image#${imageName}#g" k8s-deployment-service.yaml
+sed -i "s#placeholder-image#${imageName}#g" ./kube-manifests/k8s-deployment-service.yaml
 kubectl -n default apply -f ./kube-manifests/
